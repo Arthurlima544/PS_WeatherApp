d7c539b4900b5e7118429a777895dc7e838fda95
Arthurlima544 <arthurlindaolima544@gmail.com>
2023-10-30 08:20:55 -0300

    feat: add assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +10,24 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      home: Container(),
+      theme: ThemeData(
+        primaryColor: Color.lerp(
+          const Color(0xFF352163),
+          Color.lerp(
+            const Color(0xFF331972),
+            const Color(0xFF33143C),
+            0.5,
+          ),
+          0.5,
+        ),
+        textTheme: TextTheme(
+          titleMedium: GoogleFonts.poppins(
+            color: Colors.white,
+            fontSize: 24,
+            fontWeight: FontWeight.w500,
+          ),
         ),
       ),
     );
